33bc37f3430a2eb58d87a42537dd3db58cea9633
gstgrace <105472929+gstgrace@users.noreply.github.com>
2023-09-10 21:19:21 -0700

    Update day-2.js

--- day-2.js
+++ day-2.js
@@ -1,1 +1,19 @@
+Proof Techniques I (Visual Representations）
+
+A graph speaks 1000 lines of words
+
+The mathematics of Permutations and Combinations. Note: In a permutation, the order matters versus a combination in which the order does not matter.
+quick solution: https://nrich.maths.org/6708/solution
+more advanced formula using factorial: https://www.mathsisfun.com/combinatorics/combinations-permutations.html
+
+Six degrees of separation （by American sociologist Stanley Milgram in the 1960s. )
+The implementation of that using Graph Theory.
+https://medium.com/@melnock/graph-theory-and-basic-algorithms-65b41da68f27
+Try it out myself: https://oracleofbacon.org/help.php
+
+the mathematical field of Graph Theory is foundational to Computer Science (e.g. shortest path algorithms), with numerous applications to chemistry, social networks, chip design, molecular biology, optimal scheduling, etc.
+A well-known application of Graph Theory is Facebook, since Facebook is an example of a “social network”. Each person is a vertex (point), and each friendship/connection is an edge (line). Facebook recommends you to be friends with someone if you and that person have lots of mutual friendships.
+
+Key Takeaway: We can solve many complex Computer Science problems by representing the information visually, and using a table or a graph to uncover patterns and structure.
+  
 
