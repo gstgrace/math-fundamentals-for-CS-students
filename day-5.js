bbf855a5f47fe6063a3b5f7d1c424c06b30864bd
gstgrace <105472929+gstgrace@users.noreply.github.com>
2023-09-15 16:02:59 -0700

    Update day-5.js

--- day-5.js
+++ day-5.js
@@ -1,1 +1,19 @@
+Understanding probability and Bayes’ Theorem
 
+check out
+https://www.youtube.com/watch?v=lG4VkPoG3ko&ab_channel=3Blue1Brown
+https://www.mathsisfun.com/data/probability-false-negatives-positives.html
+This problem helps us understand CONDITIONAL PROBABILITY, specifically a formula known as Bayes’ Theorem
+https://seeing-theory.brown.edu/bayesian-inference/index.html
+
+What's Red Card Black Card Problem?
+https://www.wmbriggs.com/post/2711/
+
+What's natural language processing?
+
+What's Conjunction fallacy? (Linda's ProblemO
+It's popularized by Nobel Prize winning economist Daniel Kahneman, and appears in his best-selling book Thinking Fast and Slow. For more information on this problem, check out
+https://en.wikipedia.org/wiki/Conjunction_fallacy
+
+Many AI systems depend on probabilities to make predictions and recommend decisions. Bayes’ Theorem enables us to calculate the probability of our hypothesis given the evidence we have.
+  The “probabilistic thinking” has numerous AI applications including self-driving cars and automated translation apps like Google Translate.
